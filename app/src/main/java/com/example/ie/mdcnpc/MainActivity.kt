18d9e1bf06c886bc21236528ecc7725298755983
Sabdo182 <ienpc2020@gmail.com>
2021-11-17 12:14:34 +0700

    First init

---
+++ app/src/main/java/com/example/ie/mdcnpc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ie.mdcnpc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
